4c9b2a2cda26c1c1367f8fc4ede21eb095aa0c62
marcossadu <masadu@outlook.es>
2023-06-01 14:30:54 +0200

    gramys

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
 import "./App.css";
+import contacts from "./contacts.json"
+
+
 
 function App() {
+  const[allContacts, setAllContacts]= useState([...contacts])
+  const[removeContacts, setRemoveContacts]=useState()
+
+  function HandleClickRemove(){
+    setRemoveContacts(removeContacts)
+  
+  }
+  
+  
   return (
     <div className="App">
-      <h1>LAB | React IronContacts</h1>
+   <table>
+        <thead>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+          <th>Actions</th>
+        </thead>
+        <tbody>
+          {allContacts.map(c => (<tr>
+            <td><img style={{ width: 100 }} src={c.pictureUrl} alt={c.name}></img></td>
+            <td>{c.name}</td>
+            <td>{c.popularity.toFixed(2)}</td>
+            <td>{c.wonOscar===true ? "🏆":""}{c.wonOscar}</td>
+            <td>{c.wonEmmy===true ? "🌟":""}{c.wonEmmy}</td>
+            <td>
+              <button>{HandleClickRemove}Delete</button>
+              </td>
+          </tr>))}
+        </tbody>
+      </table>
     </div>
   );
 }
